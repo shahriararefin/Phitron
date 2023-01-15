796730bc269facad4c7575775c5b54729cc76118
shahriararefin <shahriararefin7@gmail.com>
2023-01-15 17:36:33 +0600

    Done

--- Module 21.5/p5.c
+++ Module 21.5/p5.c
@@ -1,7 +1,42 @@
 #include<stdio.h>
 
+    void op(char c,int a, int b){
+
+    int sum=0; 
+        
+    if (c== '+')
+    {
+        sum= a+b;
+    }
+    else if (c == '-')
+    {
+        sum= a-b;
+    }
+    else if (c== '*')
+    {
+        sum= a*b;
+    }
+    else if (c== '/')
+    {
+        sum= a/b;
+    }
+    
+    printf("%d", sum);
+
+    }
+
+
 int main(){
 
+    char c;
+    scanf("%c", &c);
+
+    int a,b;
+
+    scanf("%d %d", &a, &b);
+
+    
+    op(c,a,b);
 
     return 0;
 }
