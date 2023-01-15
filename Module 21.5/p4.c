bb0cc0b73e4b1a5212fa2c61d4ef0c9cd7b7c212
shahriararefin <shahriararefin7@gmail.com>
2023-01-15 16:46:25 +0600

    Done

--- Module 21.5/p4.c
+++ Module 21.5/p4.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
 
+void alleven(int n){
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (i%2==0)
+        {
+            printf("%d ", i);
+        }
+        
+    }
+
+}
+
 int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    
+    alleven(n);
+    
+    
 
     return 0;
 }
