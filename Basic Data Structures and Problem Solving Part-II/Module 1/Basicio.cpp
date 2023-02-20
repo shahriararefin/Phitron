bf348dfe298946dab89f24848ab233ad2211edb3
shahriararefin <shahriararefin7@gmail.com>
2023-02-20 20:07:43 +0600

    c++ inp out

---
+++ Basic Data Structures and Problem Solving Part-II/Module 1/Basicio.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x,y;
+    cin >> x >> y;
+
+    cout << x << endl << y;
+
+    char ch;
+    float f;
+    double d;
+
+    cin >> ch >> f >> d;
+
+    
+
+    return 0;
+}
---
+++ Basic Data Structures and Problem Solving Part-II/Module 1/firstcode.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    cout<<"Hello World"<<endl;
+
+    string s;
+    string str= "Soo Easy";
+
+    cin >> s;
+
+    cout<< s<<endl<<str;
+
+    return 0;
+}
